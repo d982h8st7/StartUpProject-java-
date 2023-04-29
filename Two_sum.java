9896fc224b133d782a917def842cf331773a644d
d982h8st7 <sinnarver15@gmail.com>
2023-04-30 01:16:31 +0900

    아시발

---
+++ Two_sum.java
@@ -0,0 +1,11 @@
+public class Two_sum{
+    public static void main(String[] args){
+        //두개의 정수를 더하여 출력하는 자바 프로그램을 만들어 보자 | 변수 : a, b ,sum 총 3개
+        int a;
+        int b;
+        int sum;
+        a = 1;b = 1;
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
