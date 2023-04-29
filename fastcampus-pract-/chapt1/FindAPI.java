c7b63010ab7aab5ef71472919d8fae5135c12de9
d982h8st7 <sinnarver15@gmail.com>
2023-04-30 06:11:23 +0900

    String이 있는게어디임ㅇㅇ

---
+++ fastcampus-pract-/chapt1/FindAPI.java
@@ -0,0 +1,8 @@
+public class FindAPI {
+    public static void main(String[] args) {
+    java.lang.String s;
+    s = "APPLE";
+        System.out.println("s = " + s);
+        System.out.println(s.length());
+    }
+}
