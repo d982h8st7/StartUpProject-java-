1c45d22c306a4807d76931ca68d4aa2875d68c8d
d982h8st7 <sinnarver15@gmail.com>
2023-04-30 04:14:33 +0900

    gkTLqkf

---
+++ fastcampus-pract-/chapt1/CarTest.java
@@ -0,0 +1,23 @@
+public class CarTest {
+    public static void main(String[] args) {
+        String model;
+        long distance;
+        long price;
+        String company;
+        char type;
+        boolean auto;
+        int year;
+        float gasmi;
+
+        model = "BMW528i";
+        distance = 100000;
+        price = 9000000000000L;
+        company = "BMW";
+        type = 'A';
+        auto = true;
+        year = 2010;
+        gasmi = 12.5f;
+        System.out.println(model+distance+price+company+type+auto+year+gasmi);
+        //좆바씹새끼
+    }
+}
