2b92863bbf21f3a8008b378be4ce22579472747d
d982h8st7 <sinnarver15@gmail.com>
2023-05-01 14:59:06 +0900

    add toBinaryString

--- fastcampus-pract-/chapt1/BinaryExam.java
+++ fastcampus-pract-/chapt1/BinaryExam.java
@@ -2,6 +2,9 @@ public class BinaryExam {
     public static void main(String[] args) {
         // 123을 decimal, binary, octal, hexa로 출력
         // 123's bin = 0111 1011
+        int data = 123;
+        String binary1 = Integer.toBinaryString(data);
+        System.out.println("binary1 = " + binary1);
         int decimal = 123;
         System.out.println("decimal = " + decimal);
         int binary = 0b01111011;
