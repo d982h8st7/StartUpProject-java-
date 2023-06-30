750f13fd949c770eedd853791c7f8cacf6fb2697
d982h8st7 <sinnarver15@gmail.com>
2023-06-30 17:42:49 +0900

    fastcampus learning java
    Signed-off-by: d982h8st7 <sinnarver15@gmail.com>

---
+++ fastcampus-pract-/chapt1/ScannerTest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("정수를 입력하세요: ");
+        int num = scan.nextInt();
+        System.out.println("num = " + num);
+
+        System.out.print("실수를 입력하세요: ");
+        float f=scan.nextFloat();
+        System.out.println("f = " + f);
+
+        System.out.print("문자열을 입력하세요: ");
+        String str = scan.nextLine();
+        System.out.println("str = " + str);
+    }
+}
